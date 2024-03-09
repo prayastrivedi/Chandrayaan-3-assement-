ae5938f19ae5ff5237af657bc4f90555f1ce6c32
prayas-mobikasa <prayastrived.in@gmail.com>
2024-03-09 22:48:19 +0530

    added fuction for executing command

--- spacecraft.ts
+++ spacecraft.ts
@@ -5,6 +5,24 @@ class Spacecraft {
     this.position = { x, y, z };
     this.direction = direction;
   }
+
+  executeCommands(commands: string[]): void {
+    for (const command of commands) {
+      if (command === "f") {
+        this.moveForward();
+      } else if (command === "b") {
+        this.moveBackward();
+      } else if (command === "r") {
+        this.turnRight();
+      } else if (command === "l") {
+        this.turnLeft();
+      } else if (command === "u") {
+        this.turnUp();
+      } else if (command === "d") {
+        this.turnDown();
+      }
+    }
+  }
   moveForward() {}
 
   moveBackward() {}
